3461fe3a767ec0f72de11bb43d7687321bc2438a
Nishant Bhakta <bhaktanishant@gmail.com>
2017-02-17 21:20:42 +0530

    "Menu" and "Popup Menu" added

--- app/src/main/java/com/example/nishant/kyalanahai2/MainActivity.java
+++ app/src/main/java/com/example/nishant/kyalanahai2/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.nishant.kyalanahai2;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String saveString = "";
     private ListView listView;
-    private static List<String> listArray = new ArrayList<String>();
+    private static List<String> listArray = new ArrayList<>();
     private SaveListInSharedPreferences saveListInSharedPreferences = new SaveListInSharedPreferences();
 
     @Override
@@ -28,10 +35,9 @@ public class MainActivity extends AppCompatActivity {
         String savedData = settings.getString(saveString, "");
         if (!savedData.isEmpty()){
             listArray = saveListInSharedPreferences.getList(savedData);
-            Log.d("isRunning : ", "True" );
         }
 
-        listView = (ListView)findViewById(R.id.itemList);
+        listView = (ListView)findViewById(android.R.id.list);
         Button button = (Button)findViewById(R.id.addButton);
 
         if (!listArray.isEmpty()) {
@@ -53,18 +59,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button clearButton = (Button)findViewById(R.id.clearButton);
-        clearButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                listArray.clear();
-                listView.setAdapter(null);
-            }
-        });
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                        final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+                        popupMenu.inflate(R.menu.popup_menu);
+                        popupMenu.setOnMenuItemClickListener(
+                                new PopupMenu.OnMenuItemClickListener() {
+                                    @Override
+                                    public boolean onMenuItemClick(MenuItem menuItem) {
+                                        switch (menuItem.getItemId()){
+                                            case R.id.popup_menu_mil_gaya:
+                                                view.setBackgroundColor(Color.parseColor("#AAFF7F"));
+                                                return true;
+                                            case R.id.popup_menu_nahi_mila:
+                                                view.setBackgroundColor(Color.parseColor("#FFAAAA"));
+                                                return true;
+                                            case R.id.popup_menu_delete_saamaan:
+                                                listArray.remove(position);
+                                                createAndSetAdapter();
+                                                return true;
+                                            default:
+                                                return false;
+                                        }
+                                    }
+                                }
+                        );
+                        popupMenu.show();
+                    }
+                }
+        );
     }
 
     public void createAndSetAdapter(){
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listArray);
         listView.setAdapter(adapter);
     }
 
@@ -77,6 +106,26 @@ public class MainActivity extends AppCompatActivity {
             SharedPreferences.Editor editor = settings.edit();
             editor.putString(saveString, listAsString);
             editor.apply();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        if (menuItem.getItemId() == R.id.main_menu_creator){
+            Toast.makeText(getApplicationContext(), "__/\\__Hello! This is Nishant Bhakta", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(menuItem.getItemId() == R.id.clearButton){
+            listArray.clear();
+            listView.setAdapter(null);
+            return true;
+        }else {
+            return false;
         }
     }
 }
